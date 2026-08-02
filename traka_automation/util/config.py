b2e0b0ed7d44e6c4c30b51084c6e375917cd131c
Wouter van Harten <wouter@woutervanharten.nl>
2026-08-02 12:38:25 +0200

    refactor: Config file to lazy loaded dict
    Signed-off-by: Wouter van Harten <wouter@woutervanharten.nl>

--- traka_automation/util/config.py
+++ traka_automation/util/config.py
@@ -1,9 +1,29 @@
 import yaml
 
+DEFAULT_SECRETS_FILE = "/onedrive/conf/secrets.yaml"
 
-def get_config():
-    with open("/onedrive/conf/secrets.yaml", "r") as f:
+
+class Config:
+    """Class to hold the configuration.
+
+    It lazy loads the secrets file. """
+
+    config: dict | None = None
+
+    def __init__(self, secrets_file: str = DEFAULT_SECRETS_FILE):
+        self.secrets_file = secrets_file
+
+    def __getitem__(self, item):
+        """Get a value from the config."""
+        if self.config is None:
+            self.config = get_config(self.secrets_file)
+        assert self.config is not None
+        return self.config[item]
+
+
+def get_config(secrets_file: str):
+    with open(secrets_file, "r") as f:
         return yaml.safe_load(f.read())
 
 
-secrets_config = get_config()
+secrets_config = Config()
