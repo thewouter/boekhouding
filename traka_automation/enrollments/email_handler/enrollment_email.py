8b7b4da89ff21939d218eca18da7695b3db79120
Wouter van Harten <wouter@woutervanharten.nl>
2026-08-23 15:34:58 +0200

    feat: add option for payment-free forms
    Signed-off-by: Wouter van Harten <wouter@woutervanharten.nl>

--- traka_automation/enrollments/email_handler/enrollment_email.py
+++ traka_automation/enrollments/email_handler/enrollment_email.py
@@ -14,17 +14,29 @@ def generate_enrollment_email(enrollment_form: EnrollmentWebForm) -> str:
         autoescape=True,
     )
 
-    template = env.get_template("enrollment_confirmation.html")
-    html = template.render(
-        participant_names=enrollment_form.combined_names,
-        camp_name=enrollment_form.camp.name,
-        payment_url=enrollment_form.payment_link.payment_link,
-        amount=enrollment_form.total_price,
-        signature_name="Wouter van Harten",
-        signature_title="Penningmeester Plusscoutskring Trapperskamp",
-        logo_url="https://next.trapperskamp.com/processed_images/trapperskamp-vught.a83be22bfa0f671b.webp",
-        camp_start_date=enrollment_form.camp.start_date_string,
-        camp_end_date=enrollment_form.camp.end_date_string,
-    )
+    if enrollment_form.has_payment_info:
+        template = env.get_template("enrollment_confirmation.html")
+        html = template.render(
+            participant_names=enrollment_form.combined_names,
+            camp_name=enrollment_form.camp.name,
+            payment_url=enrollment_form.payment_link.payment_link,  # type: ignore
+            amount=enrollment_form.total_price,
+            signature_name="Wouter van Harten",
+            signature_title="Penningmeester Plusscoutskring Trapperskamp",
+            logo_url="https://next.trapperskamp.com/processed_images/trapperskamp-vught.a83be22bfa0f671b.webp",
+            camp_start_date=enrollment_form.camp.start_date_string,
+            camp_end_date=enrollment_form.camp.end_date_string,
+        )
+    else:
+        template = env.get_template("enrollment_confirmation_no_payment.html")
+        html = template.render(
+            participant_names=enrollment_form.combined_names,
+            camp_name=enrollment_form.camp.name,
+            signature_name="Wouter van Harten",
+            signature_title="Penningmeester Plusscoutskring Trapperskamp",
+            logo_url="https://next.trapperskamp.com/processed_images/trapperskamp-vught.a83be22bfa0f671b.webp",
+            camp_start_date=enrollment_form.camp.start_date_string,
+            camp_end_date=enrollment_form.camp.end_date_string,
+        )
 
     return html
--- traka_automation/enrollments/models/enrollment_web_form.py
+++ traka_automation/enrollments/models/enrollment_web_form.py
@@ -18,9 +18,7 @@ class EnrollmentWebForm(BaseModel):
     uuid: str
     payment_link_cache: PaymentLink | None = None
 
-    model_config = ConfigDict(
-        arbitrary_types_allowed=True
-    )  #  To prevent generating many payment links
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
     @classmethod
     def from_json(cls, json_data: dict, uuid) -> EnrollmentWebForm:
@@ -47,7 +45,9 @@ class EnrollmentWebForm(BaseModel):
     @property
     def total_price(self):
         """The total price of the enrollment."""
-        return sum([p.camp.price for p in self.participants])
+        return sum(
+            [p.camp.price for p in self.participants if p.camp.price is not None]
+        )
 
     @property
     def combined_names(self):
@@ -60,8 +60,10 @@ class EnrollmentWebForm(BaseModel):
         return combined_names
 
     @property
-    def payment_link(self) -> PaymentLink:
+    def payment_link(self) -> PaymentLink | None:
         """Get the payment link for the Enrollment."""
+        if self.camp.price is None:
+            return None
         if self.payment_link_cache is None:
             self.payment_link_cache = generate_payment_link(
                 self.combined_names,
@@ -87,3 +89,7 @@ class EnrollmentWebForm(BaseModel):
         filename = os.path.join(folder, f"{self.uuid}.json")
         with open(filename, "w") as f:
             f.write(self.json_representation)
+
+    @property
+    def has_payment_info(self):
+        return self.camp.price is not None
