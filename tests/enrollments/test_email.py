b3314ca6ea0e2d6b6bfa0e31c60658b30e50850d
Wouter van Harten <wouter@woutervanharten.nl>
2026-09-13 12:14:49 +0200

    fix: fix bug in html generation code where model was inserted instead of string link.
    Added test to test verify this
    
    Signed-off-by: Wouter van Harten <wouter@woutervanharten.nl>

--- tests/enrollments/test_email.py
+++ tests/enrollments/test_email.py
@@ -16,7 +16,17 @@ def test_enrollment_email(example_enrollment_web_form: EnrollmentWebForm):
     html5parser.parse(html)
 
 
+def test_enrollment_email_no(example_enrollment_web_form_no_price: EnrollmentWebForm):
+    """Test whether the enrollment email generation generates valid HTML5."""
+    html = generate_enrollment_email(
+        example_enrollment_web_form_no_price,
+    )
+    html5parser = html5lib.HTMLParser(strict=True)
+    html5parser.parse(html)
+
+
 def test_enroll_email_content_keywords(example_enrollment_web_form: EnrollmentWebForm):
+    """Test whether the enrollment email generation contains the expected keywords."""
     html = generate_enrollment_email(example_enrollment_web_form)
     assert example_enrollment_web_form.camp.start_date_string in html
     assert example_enrollment_web_form.camp.end_date_string in html
@@ -26,3 +36,16 @@ def test_enroll_email_content_keywords(example_enrollment_web_form: EnrollmentWe
     assert f"{example_enrollment_web_form.total_price:0.2f}" in html
     assert get_payment_link(example_enrollment_web_form).payment_link in html  # type: ignore
     assert example_enrollment_web_form.combined_names in html
+
+
+def test_enroll_email_content_keywords_no_price(example_enrollment_web_form_no_price: EnrollmentWebForm):
+    """Test whether the enrollment email generation contains the expected keywords."""
+    html = generate_enrollment_email(example_enrollment_web_form_no_price)
+    assert example_enrollment_web_form_no_price.camp.start_date_string in html
+    assert example_enrollment_web_form_no_price.camp.end_date_string in html
+    assert example_enrollment_web_form_no_price.camp.name in html
+    for participant in example_enrollment_web_form_no_price.participants:
+        assert participant.name in html
+    assert example_enrollment_web_form_no_price.combined_names in html
+    assert "€" not in html
+    assert "betaal" not in html
